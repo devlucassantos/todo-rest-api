b285bdd9bf723ca318f45969f7526592445e48cd
Lucas Santos <lucasrsantos.dev@gmail.com>
2022-04-23 11:20:40 -0300

    Resources

---
+++ res/sql/init_db.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE todo_db;
+
+\connect todo_db;
+
+CREATE TABLE user_account
+(
+     id       SERIAL       PRIMARY KEY,
+     name     VARCHAR(50)  NOT NULL,
+     email    VARCHAR(50)  UNIQUE,
+     password VARCHAR(200) NOT NULL,
+     hash     VARCHAR(600) NOT NULL,
+     token    VARCHAR(300) NOT NULL
+);
+
+CREATE TABLE collection
+(
+    id   SERIAL      PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+
+    user_id INT NOT NULL,
+
+    CONSTRAINT collection_user_fk FOREIGN KEY (user_id) REFERENCES user_account (id)
+);
+
+CREATE TABLE item
+(
+    id          SERIAL      PRIMARY KEY,
+    description VARCHAR(50) NOT NULL,
+    marked      BOOLEAN     NOT NULL,
+
+    user_id       INT NOT NULL,
+    collection_id INT,
+
+    CONSTRAINT item_user_fk       FOREIGN KEY (user_id)       REFERENCES user_account (id),
+    CONSTRAINT item_collection_fk FOREIGN KEY (collection_id) REFERENCES collection   (id)
+);
+
+CREATE DATABASE todo_db_test WITH TEMPLATE todo_db OWNER postgres;
---
+++ res/sql/load_fixtures.sql
@@ -0,0 +1,16 @@
+\connect todo_db;
+
+COPY user_account (id, name, email, password, hash, token)
+FROM '/mydata/fixtures/user_account.csv'
+CSV HEADER;
+SELECT SETVAL('user_account_id_seq', (SELECT MAX(id) FROM user_account));
+
+COPY collection (id, name, user_id)
+FROM '/mydata/fixtures/collection.csv'
+CSV HEADER;
+SELECT SETVAL('collection_id_seq', (SELECT MAX(id) FROM collection));
+
+COPY item (id, description, marked, user_id, collection_id)
+FROM '/mydata/fixtures/item.csv'
+CSV HEADER;
+SELECT SETVAL('item_id_seq', (SELECT MAX(id) FROM item));
