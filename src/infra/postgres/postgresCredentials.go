726ff45a8a5f0dcf35540a421d4b49a5cb0003c6
Lucas Santos <lucasrsantos.dev@gmail.com>
2025-02-25 07:24:17 -0300

    Updated the process of pushing docker images to Docker Hub

--- src/infra/postgres/postgresCredentials.go
+++ src/infra/postgres/postgresCredentials.go
@@ -6,11 +6,11 @@ import (
 )
 
 func getPostgresConnectionURI() (string, error) {
-	user := os.Getenv("POSTGRESQL_USER")
-	password := os.Getenv("POSTGRESQL_PASSWORD")
-	host := os.Getenv("POSTGRESQL_HOST")
-	port := os.Getenv("POSTGRESQL_PORT")
-	dbName := os.Getenv("POSTGRESQL_DB")
+	user := os.Getenv("POSTGRES_SERVICE_USER")
+	password := os.Getenv("POSTGRES_SERVICE_USER_PASSWORD")
+	host := os.Getenv("POSTGRES_HOST")
+	port := os.Getenv("POSTGRES_PORT")
+	dbName := os.Getenv("POSTGRES_DB")
 
 	connectionUri := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", user, password, host, port, dbName)
 	return connectionUri, nil
